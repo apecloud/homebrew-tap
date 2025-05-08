d7207ca8bbd14f5ef560b2cf96b6c61cd53fc4ca
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2025-05-08 10:34:18 +0800

    chore: fix release homebrew tap (#8)
    * chore: fix release homebrew tap
    
    * chore: auto update kbcli version v0.9.3
    
    * chore: auto update kbcli version v0.5.1
    
    * chore: change kbcli release sha256
    
    * chore: add update_formula_version workflow
    
    ---------
    
    Co-authored-by: JashBook <JashBook@users.noreply.github.com>

--- .github/utils/update_formula_version.sh
+++ .github/utils/update_formula_version.sh
@@ -34,11 +34,18 @@ update_file_with_line_num() {
 }
 
 update_formula_version() {
+    formula_version_file=${formula_file/.rb/@$VERSION.rb}
+    if [[ "$VERSION" == *.*.* && "$VERSION" != *.*.*-* && ! -f $formula_version_file ]]; then
+        cp -r $formula_file $formula_version_file
+        git add $formula_version_file
+        update_file "class Kbcli < Formula" "class KbcliAT${VERSION//./} < Formula" $formula_version_file
+    fi
+
     # get formula kbcli.rb version
-    formula_version=$( cat $formula_file | grep version | awk 'NR==1{print $2}' | sed 's/\"//g' )
+    formula_version=$( cat $formula_version_file | grep version | awk 'NR==1{print $2}' | sed 's/\"//g' )
     # update formula kbcli.rb version
     echo "update formula_version:"$formula_version" to release_version:"$VERSION
-    update_file $formula_version $VERSION $formula_file
+    update_file $formula_version $VERSION $formula_version_file
     url_sha256=""
     line_num=0
     while read -r readline; do
@@ -51,20 +58,10 @@ update_formula_version() {
         elif [[ "$readline" == *"sha256"* ]];then
             sha256=$( echo "$readline" | awk '{print $2}' | sed 's/\"//g' )
             echo "update sha256:"$sha256" to url_sha256:"$url_sha256
-            update_file_with_line_num $sha256 $url_sha256 $formula_file $line_num
+            update_file_with_line_num $sha256 $url_sha256 $formula_version_file $line_num
             url_sha256=""
         fi
-    done < $formula_file
+    done < $formula_version_file
 }
 
 update_formula_version
-
-if [[ "$VERSION" == *.*.* && "$VERSION" != *.*.*-* ]]; then
-    formula_version_file=${formula_file/.rb/@$VERSION.rb}
-    if [[ ! -f $formula_version_file ]]; then
-        cp -r $formula_file $formula_version_file
-        git add $formula_version_file
-        update_file "class Kbcli < Formula" "class KbcliAT${VERSION//./} < Formula" $formula_version_file
-    fi
-fi
-
