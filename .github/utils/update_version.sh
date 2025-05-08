994aef2c237e47a7272ce7b2c620954ce9265068
huangzhangshu <huangzhangshu@apecloud.com>
2025-05-08 10:03:04 +0800

    chore: fix release homebrew tap

--- .github/utils/update_version.sh
+++ .github/utils/update_version.sh
@@ -22,6 +22,21 @@ update_file() {
     esac
 }
 
+update_file_with_line_num() {
+    echo $1
+    echo $2
+    echo $3
+    echo $4
+    case $OS in
+        darwin)
+            sed -i '' "$4s/$1/$2/g" $3
+        ;;
+        *)
+            sed -i "$4s/$1/$2/g" $3
+        ;;
+    esac
+}
+
 update_formula_version() {
     # get formula kbcli.rb version
     formula_version=$( cat $formula_file | grep version | awk 'NR==1{print $2}' | sed 's/\"//g' )
@@ -29,15 +44,19 @@ update_formula_version() {
     echo "update formula_version:"$formula_version" to release_version:"$VERSION
     update_file $formula_version $VERSION $formula_file
     url_sha256=""
+    line_num=0
     while read -r readline; do
+        line_num=$(( line_num + 1 ))
         if [[ "$readline" == *"url"* ]];then
             url=$( echo "$readline" | awk '{print $2}' | sed 's/\"//g' )
             echo "get $url sha256..."
             url_sha256=$( curl -sL $url | shasum -a 256 | cut -d " " -f 1 )
+            echo "url_sha256:"$url_sha256
         elif [[ "$readline" == *"sha256"* ]];then
             sha256=$( echo "$readline" | awk '{print $2}' | sed 's/\"//g' )
             echo "update sha256:"$sha256" to url_sha256:"$url_sha256
-            update_file $sha256 $url_sha256 $formula_file
+            update_file_with_line_num $sha256 $url_sha256 $formula_file $line_num
+            url_sha256=""
         fi
     done < $formula_file
 }
